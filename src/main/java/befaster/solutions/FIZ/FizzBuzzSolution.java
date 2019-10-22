6a4fce9c997a95acc90216755b620dc63d505499
sbx_user1051 <sbx_user1051@169.254.129.9>
2019-10-22 22:04:19 +0000

    Tue Oct 22 22:04:19 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,24 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        String message="";
+        if (number%3 == 0) {
+            message ="fizz";
+        }
+        if(number%5 == 0) {
+            if(message.isEmpty()) {
+                message ="buzz";
+            }else {
+                message += " buzz";
+            }
+        }
+        if(message.isEmpty()) {
+            message = number.toString();
+        }
+        return message;
     }
 
 }
+
--- src/test/java/befaster/solutions/FIZ/FizBuzTest.java
+++ src/test/java/befaster/solutions/FIZ/FizBuzTest.java
@@ -1,12 +1,15 @@
 package befaster.solutions.FIZ;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FizBuzTest {
 
     @Test
     public void testFizbuz() {
-        
+        FizzBuzzSolution fizbuz = new FizzBuzzSolution();
+        assertEquals("fizz", fizbuz.fizzBuzz(9));
     }
 
 }
+
