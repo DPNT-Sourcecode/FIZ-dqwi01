4725b0dd39e97167ecf35dbccd6590b7402e13c3
sbx_user1051 <sbx_user1051@169.254.129.9>
2019-10-22 22:06:14 +0000

    Tue Oct 22 22:06:14 UTC 2019

--- src/test/java/befaster/solutions/FIZ/FizBuzTest.java
+++ src/test/java/befaster/solutions/FIZ/FizBuzTest.java
@@ -6,10 +6,23 @@ import org.junit.Test;
 public class FizBuzTest {
 
     @Test
-    public void testFizbuz() {
+    public void testFizz() {
         FizzBuzzSolution fizbuz = new FizzBuzzSolution();
         assertEquals("fizz", fizbuz.fizzBuzz(9));
     }
 
+    @Test
+    public void testBuzz() {
+        FizzBuzzSolution fizbuz = new FizzBuzzSolution();
+        assertEquals("buzz", fizbuz.fizzBuzz(10));
+    }
+    
+    @Test
+    public void testFizzBuzz() {
+        FizzBuzzSolution fizbuz = new FizzBuzzSolution();
+        assertEquals("fizz buzz", fizbuz.fizzBuzz(15));
+    }
+
 }
+
 
